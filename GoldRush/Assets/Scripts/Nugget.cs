8f788f7de28b53c98d34a9f79ec5fdb632cd75c8
jackferrari <jackxferrari@gmail.com>
2019-11-25 16:52:09 -0500

    who's heard of a filter?

--- GoldRush/Assets/Scripts/Nugget.cs
+++ GoldRush/Assets/Scripts/Nugget.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Nugget : MonoBehaviour {
+public class Nugget : MonoBehaviour
+{
 
     public float x;
     public float y;
 
     public float rotZ;
 
-	// Update is called once per frame
-	void Update () { 
+    // Update is called once per frame
+    void Update()
+    {
         transform.Translate(new Vector2(x, y), Space.World);
         transform.Rotate(0, 0, rotZ, Space.Self);
-	}
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
--- GoldRush/Assets/Scripts/SpawnNugget.cs
+++ GoldRush/Assets/Scripts/SpawnNugget.cs
@@ -2,16 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnNugget : MonoBehaviour {
+public class SpawnNugget : MonoBehaviour
+{
 
     public GameObject nugget;
-	
-	// Update is called once per frame
-	void Update () {
-        int spawnNumber = Random.Range(0, 20);
-        if (spawnNumber > 12)
+    public int wait;
+
+    private void Start()
+    {
+        wait = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wait > 0)
         {
-            Instantiate(nugget, transform.position, transform.rotation);
+            wait--;
         }
-	}
+        else
+        {
+            int spawnNumber = Random.Range(0, 200);
+            if (spawnNumber > 10)
+            {
+                Instantiate(nugget, transform.position, transform.rotation);
+            }
+            wait = Random.Range(50, 200);
+        }
+
+    }
 }
